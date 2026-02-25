6050c6f1cfaa73dd8acd07cf15ad346e025a3b09
Roger_iin <rogersimoes@hotmail.com>
2026-02-25 14:51:54 -0300

    feat: Adiciona p dto RegisterRequest com as validações para cadastro de usuário

---
+++ src/main/java/com/financemanager/finance_manager/dto/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.financemanager.finance_manager.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+
+        @NotBlank(message = "Nome é obrigatório")
+        @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+        String name,
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "E-mail inválido")
+        String email,
+
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 8, message = "A senha deve ter no minimo 8 caracteres")
+        String password
+
+) {}
