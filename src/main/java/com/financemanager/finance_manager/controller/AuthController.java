ac704cda9b61b8156f303600a88775816e2bc30a
Roger_iin <rogersimoes@hotmail.com>
2026-02-27 11:46:18 -0300

    feat: Adiciona o Contoller de Autenhticação com as rotas de login e registro

---
+++ src/main/java/com/financemanager/finance_manager/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.financemanager.finance_manager.controller;
+
+import com.financemanager.finance_manager.dto.AuthResponse;
+import com.financemanager.finance_manager.dto.RegisterRequest;
+import com.financemanager.finance_manager.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register/")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
