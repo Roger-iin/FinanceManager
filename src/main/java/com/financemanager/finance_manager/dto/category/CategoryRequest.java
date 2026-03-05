c32a4807f0804551697b8d6b5a0ef8915f7cb1d8
Roger_iin <rogersimoes@hotmail.com>
2026-03-05 14:27:18 -0300

    feat Adiciona o DTO Request de Category

---
+++ src/main/java/com/financemanager/finance_manager/dto/category/CategoryRequest.java
@@ -0,0 +1,23 @@
+package com.financemanager.finance_manager.dto.category;
+
+import com.financemanager.finance_manager.model.enums.TransactionType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CategoryRequest(
+
+        @NotBlank(message = "Nome é obrigatório")
+        @Size(max = 100, message = "Nome deve ter no máximo 100 caracteres")
+        String nome,
+
+        @NotNull(message = "Tipo é obrigatorio")
+        TransactionType type,
+
+        @Size(max = 50, message = "Icono deve ter no máximo 50 caracteres")
+        String icon,
+
+        @Pattern(regexp = "^#[0-9A-Fa-f]{6}$", message = "Cor deve estar no formato hexadecimal. Ex: #8A2BE2")
+        String color
+) {}
