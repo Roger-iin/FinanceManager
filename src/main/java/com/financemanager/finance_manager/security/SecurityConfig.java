692e155a39b9254ea8d52d8a557796664e9cd6ca
Roger_iin <rogersimoes@hotmail.com>
2026-03-16 14:51:47 -0300

    feat: Configura o CORS para aceitar PATCH

--- src/main/java/com/financemanager/finance_manager/security/SecurityConfig.java
+++ src/main/java/com/financemanager/finance_manager/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://192.168.15.*:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
